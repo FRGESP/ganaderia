80de64857ae79133e2a86ad3ffe088f21a1661b2
FRGESP <f.gordilloespinoza@ugto.mx>
2024-10-26 19:21:52 -0600

    Login Front

---
+++ src/components/ui/form-login.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { z } from "zod"
+import { LoginSchema } from '@/lib/zod';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+ 
+
+function FormLogin() {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+          user: "",
+            password: "",
+        },
+      })
+      
+      function onSubmit(values: z.infer<typeof LoginSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+
+      return <div >
+        <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="user"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-xl">Usuario</FormLabel>
+              <FormControl>
+                <Input placeholder="Toque aquí para ingresar su usuario" {...field} className="border-gray-950 rounded-xl text-xl p-7"autoFocus/>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-xl">Contraseña</FormLabel>
+              <FormControl>
+                <Input placeholder="Toque aquí para ingresar su contraseña" {...field} className="border-gray-950 rounded-xl p-7 text-xl" />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="bg-acento hover:bg-acentohover w-full p-7 text-xl rounded-full py-8">Ingresar</Button>
+      </form>
+    </Form>
+      </div>
+}
+
+export default FormLogin
---
+++ src/lib/zod.tsx
@@ -0,0 +1,30 @@
+import { number, object, string } from "zod"
+ 
+export const LoginSchema = object({
+  user: string({ required_error: "Se requiere su usuario" })
+    .min(1, "Se requiere su usuario")
+    .max(50, "Su usuario debe ser menor a 50 caracteres"),
+  password: string({ required_error: "Se requiere la contraseña" })
+    .min(1, "La contraseña es requerida")
+    .min(6, "La contraseña debe tener al menos 8 caracteres")
+    .max(32, "La contraseña debe tener menos de 32 caracteres"),
+})
+
+export const RegisterSchema = object({
+    Nombre: string({ required_error: "Se requiere su nombre" })
+      .min(1, "Se requiere su nombre")
+      .max(50, "Su nombre debe ser menor a 50 caracteres"),
+    Telefono: string({ required_error: "Se requiere su telefono" })
+      .min(10, "Ingrese su telefono de 10 digitos")
+      .max(10, "Ingrese su telefono de 10 digitos"),
+    Edad: string({ required_error: "Se requiere su edad" })
+      .min(1, "Se requiere su edad")
+      .max(3, "Su edad debe ser menor a 3 caracteres"),
+    Rol: string({ required_error: "Se requiere su rol" })
+      .min(1, "Se requiere su rol")
+      .max(50, "Su rol debe ser menor a 50 caracteres"),
+    Sueldo: number({ required_error: "Se requiere su sueldo" }),
+    Estatus: string({ required_error: "Se requiere su estatus" })
+      .min(1, "Se requiere su estatus")
+      .max(50, "Su estatus debe ser menor a 50 caracteres"),
+});
