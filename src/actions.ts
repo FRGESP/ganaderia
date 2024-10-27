422d2b99b43dc1f0e0efc58fb8f37a31fc2caf89
FRGESP <f.gordilloespinoza@ugto.mx>
2024-10-27 11:01:50 -0600

    Actions

--- src/actions.ts
+++ src/actions.ts
@@ -1,67 +1,65 @@
-"use server"
+"use server";
 
-import { sessionOptions, SessionData, defaultSession } from "@/lib"
-import { getIronSession } from "iron-session"
-import { redirect } from "next/navigation"
-import { cookies } from "next/headers"
-import { LoginSchema } from '@/lib/zod';
+import { sessionOptions, SessionData, defaultSession } from "@/lib";
+import { getIronSession } from "iron-session";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { LoginSchema } from "@/lib/zod";
 
 import axios from "axios";
 
 interface Credentials {
-    user: string;
-    password: string;
+  user: string;
+  password: string;
 }
 
-export  const  getsession = async () => {
-    const session = await getIronSession<SessionData>(cookies(),sessionOptions)
+export const getsession = async () => {
+  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
 
-    if(!session.islogged){
-        session.islogged = defaultSession.islogged;
-    }
-    console.log("S");
-    return session;
-}
-export const login = async (credentials:Credentials) => {
-    console.log(credentials);
-    const session = await getsession();
+  if (!session.islogged) {
+    session.islogged = defaultSession.islogged;
+  }
+  console.log("S");
+  return session;
+};
+export const login = async (credentials: Credentials) => {
+  console.log(credentials);
+  const session = await getsession();
 
-    const response = await axios.post(`${process.env.URL}/api/users/login`, credentials);
-    const [datos] = response.data;
-    
+  const response = await axios.post(
+    `${process.env.URL}/api/users/login`,
+    credentials
+  );
+  const [datos] = response.data;
 
-    if(datos.RES !== undefined){
-        return datos.RES;
-        
-    } else {
-
+  if (datos.RES !== undefined) {
+    return datos.RES;
+  } else {
     session.userId = datos.IdUsuario;
     session.rol = datos.Rol;
     session.islogged = true;
 
-    await session.save(); 
-     await roles();
-    }
-    
+    await session.save();
+    await roles();
+  }
+};
 
-}
-
-export const roles = async () =>{
-    const session = await getsession();
-    console.log(session.rol);
-    switch(session.rol){
-        case 1:
-            redirect("/users/admin");
-            break;
-        case 2:
-            redirect("/users/almacen");
-            break;
-        case 3:
-            redirect("/users/ventas");
-            break;
-        default:
-            console.log("No se encontro el rol");
-            redirect("/");
-    }
-}
-export const logout = () => {}
+export const roles = async () => {
+  const session = await getsession();
+  console.log(session.rol);
+  switch (session.rol) {
+    case 1:
+      redirect("/users/admin");
+      break;
+    case 2:
+      redirect("/users/almacen");
+      break;
+    case 3:
+      redirect("/users/ventas");
+      break;
+    default:
+      console.log("No se encontro el rol");
+      redirect("/");
+  }
+};
+export const logout = () => {};
