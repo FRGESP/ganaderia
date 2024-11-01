f1e8194aba394af44e32a149a5b7a319233ed2aa
FRGESP <f.gordilloespinoza@ugto.mx>
2024-10-31 18:28:20 -0600

    Mejora estilo Form Edit Empleados

--- src/components/ui/buttonEdit.tsx
+++ src/components/ui/buttonEdit.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { Button } from "@/components/ui/button";
+
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -12,33 +10,6 @@ import {
 import { Edit} from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { EmpleadosEditSchema } from "@/lib/zod";
-import { Separator } from "@radix-ui/react-separator";
-import { useToast } from "@/hooks/use-toast";
-
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import axios from "axios";
-import { useRouter } from "next/navigation";
 import React, { use, useEffect } from "react";
 import { EmpleadoEditForm } from "./empleadosEditForm";
 
@@ -47,80 +18,12 @@ interface ButtonEditEmpleadosProps {
 }
 
 function ButtonEditEmpleados({id}: ButtonEditEmpleadosProps) {
-  const { toast } = useToast();
-  const router = useRouter();
   const [open, setOpen] = React.useState(false);
 
-  const handleClickEdit = () => {
-    axios.get(`/api/users/admin/empleados/${id}`).then((response) => {
-      form.reset({
-        Nombre: response.data.Nombre,
-        ApellidoPaterno: response.data.ApellidoPaterno,
-        ApellidoMaterno: response.data.ApellidoMaterno,
-        Edad: response.data.Edad,
-        Telefono: response.data.Telefono,
-        Rol: response.data.Rol.toString(),
-        Sueldo: response.data.Sueldo,
-        Estatus: response.data.Estatus.toString(),
-      });
-    });
-  };
-
-  useEffect(() => {
-    if (open) handleClickEdit();
-  }, [open]);
-
-  //Dataos default del formulario
-  const [defaultFormValues, setDefaultFormValues] = React.useState({
-    Nombre: "",
-    ApellidoPaterno: "",
-    ApellidoMaterno: "",
-    Edad: "",
-    Telefono: "",
-    Rol: "2",
-    Sueldo: 1,
-    Estatus: "1",
-  });
 
 
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof EmpleadosEditSchema>>({
-    resolver: zodResolver(EmpleadosEditSchema),
-    defaultValues: {
-      Nombre: "",
-      ApellidoPaterno: "",
-      ApellidoMaterno: "",
-      Edad: "",
-      Telefono: "",
-      Rol: "2",
-      Sueldo: 1,
-      Estatus: "1",
-    },
-  });
 
-  // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof EmpleadosEditSchema>) {
-    console.log("si llega");
 
-    const response = await axios.put(`/api/users/admin/empleados/${id}`, values);
-    if (response.data.RES !== 1) {
-      toast({
-        title: "Empelado agregado con éxito",
-        description: "El empleado se ha agregado correctamente",
-        variant: "success",
-      });
-      setOpen(false);
-      form.reset();
-      router.refresh();
-    } else {
-      toast({
-        title: "Error al agregar empleado",
-        description: "Hubo un error al agregar el empleado",
-        variant: "destructive",
-      });
-    }
-    console.log(values);
-  }
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -133,181 +36,7 @@ function ButtonEditEmpleados({id}: ButtonEditEmpleadosProps) {
           <DialogTitle>Agregar Empleado</DialogTitle>
         </DialogHeader>
         <ScrollArea className="h-[500px] w-[450px] rounded-md border p-5 m-1">
-          <div className="m-2">
-            {/* <Form {...form}>
-              <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-4"
-              >
-                <FormField
-                  control={form.control}
-                  name="Nombre"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Nombre</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder=""
-                          {...field}
-                          className="border border-[#555555]"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="ApellidoPaterno"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Apellido Paterno</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder=""
-                          {...field}
-                          className="border border-[#555555]"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="ApellidoMaterno"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Apellido Materno</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder=""
-                          {...field}
-                          className="border border-[#555555]"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="Edad"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Edad</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder=""
-                          {...field}
-                          className="border border-[#555555]"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="Telefono"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Teléfono</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder=""
-                          {...field}
-                          className="border border-[#555555]"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="Sueldo"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Sueldo</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder=""
-                          {...field}
-                          className="border border-[#555555]"
-                          onChange={(e) => field.onChange(Number(e.target.value))} // Convierte el valor a número
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="Rol"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Rol</FormLabel>
-                      <FormControl>
-                        <Select
-                          onValueChange={field.onChange}
-                          value={field.value || "1"}
-                        >
-                          <SelectTrigger className="border-[#555555] text-black w-full" >
-                            <SelectValue placeholder="Rol" />
-                          </SelectTrigger>
-                          <SelectContent className="border border-[#555555]">
-                            <SelectItem value="1">Administrador</SelectItem>
-                            <Separator />
-                            <SelectItem value="2">Almacen</SelectItem>
-                            <SelectItem value="3">Entradas</SelectItem>
-                          </SelectContent>
-                        </Select>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="Estatus"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Estatus</FormLabel>
-                      <FormControl>
-                        <Select
-                          onValueChange={field.onChange}
-                          value={field.value || "1"}
-                        >
-                          <SelectTrigger className=" border-[#555555] text-black w-full">
-                            <SelectValue placeholder="Rol" />
-                          </SelectTrigger>
-                          <SelectContent className="border border-[#555555]">
-                            <SelectItem value="1">Activo</SelectItem>
-                            <SelectItem value="2">Suspendido</SelectItem>
-                            <SelectItem value="3">Despedido</SelectItem>
-                          </SelectContent>
-                        </Select>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <Button type="submit" onClick={() => form.handleSubmit(onSubmit)()} className="bg-acento hover:bg-acentohover w-full p-4 text-md rounded-full py-6">
-                  Agregar
-                </Button>
-              </form>
-            </Form> */}
-            <EmpleadoEditForm Id={id}/>
-          </div>
+          <EmpleadoEditForm Id={id}/>
         </ScrollArea>
       </DialogContent>
     </Dialog>
--- src/components/ui/empleadosEditForm.tsx
+++ src/components/ui/empleadosEditForm.tsx
@@ -53,13 +53,13 @@ export function EmpleadoEditForm({Id}:EmpleadoProps) {
         const res = await axios.put(`/api/users/admin/empleados/${Id}`, empleado);
         if (res.data == 1) {
             toast({
-              title: "Empleado eliminado",
+              title: "Empleado Actualizado",
               description: "El empleado ha sido eliminado correctamente",
               variant: "success",
             });
           } else {
             toast({
-              title: "Error al eliminar el empleado",
+              title: "Error al actualizar el empleado",
               description: "Hubo un error al eliminar el empleado",
               variant: "destructive",
             });
@@ -70,33 +70,33 @@ export function EmpleadoEditForm({Id}:EmpleadoProps) {
         // router.refresh();
     }
   return (
-    <div>
-    <form className='bg-white shadow-md rounded-md px-8 pt-6 pb-8 mb-4' onSubmit={handlesubmit} ref={form}>
+    <div className=''>
+    <form className='bg-white shadow-md rounded-md mb-4' onSubmit={handlesubmit} ref={form}>
       <label htmlFor="Nombre" className='block text-gray-700 text-sm font-bold mb-2'>Nombre</label>
-      <input value={empleado.Nombre} type="text" id="Nombre" name="Nombre" placeholder='Nombre' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black' autoFocus/>
+      <input value={empleado.Nombre} type="text" id="Nombre" name="Nombre" placeholder='Nombre' onChange={handleChange} className=' border rounded w-full py-2 px-3 text-black border-black' autoFocus/>
 
-      <label htmlFor="ApellidoPaterno" className='block text-gray-700 text-sm font-bold mb-2' >Apellido Paterno</label>
-      <input value={empleado.ApellidoPaterno} type="text" id="ApellidoPaterno" name="ApellidoPaterno" placeholder='ApellidoPaterno' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black'/>
+      <label htmlFor="ApellidoPaterno" className='block text-gray-700 text-sm font-bold my-2' >Apellido Paterno</label>
+      <input value={empleado.ApellidoPaterno} type="text" id="ApellidoPaterno" name="ApellidoPaterno" placeholder='ApellidoPaterno' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black border-black' />
 
-      <label htmlFor="ApellidoMaterno" className='block text-gray-700 text-sm font-bold mb-2' >Apellido Materno</label>
-      <input value={empleado.ApellidoMaterno} type="text" id="ApellidoMaterno" name="ApellidoMaterno" placeholder='ApellidoMaterno' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black'/>
+      <label htmlFor="ApellidoMaterno" className='block text-gray-700 text-sm font-bold my-2' >Apellido Materno</label>
+      <input value={empleado.ApellidoMaterno} type="text" id="ApellidoMaterno" name="ApellidoMaterno" placeholder='ApellidoMaterno' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black border-black'/>
 
-      <label htmlFor="Edad" className='block text-gray-700 text-sm font-bold mb-2' >Edad</label>
-      <input value={empleado.Edad} type="text" id="Edad" name="Edad" placeholder='Edad' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black'/>
+      <label htmlFor="Edad" className='block text-gray-700 text-sm font-bold my-2' >Edad</label>
+      <input value={empleado.Edad} type="text" id="Edad" name="Edad" placeholder='Edad' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black border-black'/>
 
-      <label htmlFor="Telefono" className='block text-gray-700 text-sm font-bold mb-2' >Telefono</label>
-      <input value={empleado.Telefono} type="text" id="Telefono" name="Telefono" placeholder='Telefono' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black'/>
+      <label htmlFor="Telefono" className='block text-gray-700 text-sm font-bold my-2' >Telefono</label>
+      <input value={empleado.Telefono} type="text" id="Telefono" name="Telefono" placeholder='Telefono' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black border-black'/>
 
-      <label htmlFor="Sueldo" className='block text-gray-700 text-sm font-bold mb-2'>Sueldo</label>
-      <input value={empleado.Sueldo} type="Sueldo" id="Sueldo" name="Sueldo" placeholder='Sueldo' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black' autoFocus/>
+      <label htmlFor="Sueldo" className='block text-gray-700 text-sm font-bold my-2'>Sueldo</label>
+      <input value={empleado.Sueldo} type="Sueldo" id="Sueldo" name="Sueldo" placeholder='Sueldo' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black border-black '/>
 
-      <label htmlFor="Rol" className='block text-gray-700 text-sm font-bold mb-2' >Rol</label>
-      <input value={empleado.Rol} type="text" id="Rol" name="Rol" placeholder='Rol' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black'/>
+      <label htmlFor="Rol" className='block text-gray-700 text-sm font-bold my-2' >Rol</label>
+      <input value={empleado.Rol} type="text" id="Rol" name="Rol" placeholder='Rol' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black border-black'/>
 
-      <label htmlFor="Estatus" className='block text-gray-700 text-sm font-bold mb-2' >Estatus</label>
-      <input value={empleado.Estatus} type="Estatus" id="Estatus" name="Estatus" placeholder='Estatus' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black'/>
+      <label htmlFor="Estatus" className='block text-gray-700 text-sm font-bold my-2' >Estatus</label>
+      <input value={empleado.Estatus} type="Estatus" id="Estatus" name="Estatus" placeholder='Estatus' onChange={handleChange} className='shadow appearance-none border rounded w-full py-2 px-3 text-black border-black'/>
 
-      <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg'>Actualizar</button>
+      <button className='bg-acento hover:bg-acentohover w-full p-4 text-md rounded-full py-3 mt-4'>Actualizar</button>
 
     </form>
   </div>
