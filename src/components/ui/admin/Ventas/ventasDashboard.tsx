daee4ef3e6aaaca6adf7d32f1ee52937eee68e1f
FRGESP <f.gordilloespinoza@ugto.mx>
2024-11-26 20:36:07 -0600

    Motivo Sacrificio Ventas

--- src/components/ui/admin/Ventas/ventasDashboard.tsx
+++ src/components/ui/admin/Ventas/ventasDashboard.tsx
@@ -24,8 +24,9 @@ function VentasDashboard() {
     Meses: number;
     Clasificacion: string;
     Corral: string;
-    Peso: string;
+    Peso: number;
     PrecioCompra: number;
+    PrecioSugerido: number;
   }
 
   //Interfae de la información del corral seleccionado
@@ -35,6 +36,8 @@ function VentasDashboard() {
     Ganancia: number;
     REEMO: string;
     PrecioNumber: number;
+    IdMotivo: number;
+    Motivo: string;
   }
 
   //Interface para el corral seleccionado
@@ -71,10 +74,42 @@ function VentasDashboard() {
   const [CorralInfo, setCorralInfo] = useState<CorralInfo>();
 
   //Estado si está imprimiento
-    const [isNotPrinting, setIsNotPrinting] = useState(true);
+  const [isNotPrinting, setIsNotPrinting] = useState(true);
 
-    //Guarda la informacion del último id de la venta
-    const [lastId, setLastId] = useState(0);
+  //Guarda la informacion del último id de la venta
+  const [lastId, setLastId] = useState(0);
+
+  //Establece el estado si el motivo es sacrificio
+  const [isSacrificio, setIsSacrificio] = useState(false);
+
+  //Guarda la información del precio de cada animal
+  const [inputPrecios, setInputPrecios] = useState<{ [key: string]: number }>(
+    {}
+  );
+
+  //Calcula el total de la venta cuando es sacrificio
+  const totalSacrificio = () => {
+    let total = 0;
+    for (let i = 0; i < AnimalesCorral.length; i++) {
+      total += inputPrecios[AnimalesCorral[i].Arete];
+    }
+    inputValue.precioFinal = total;
+  };
+
+  //Controla el cambio del precio de cada animal
+  const handlePrecioChange = (e: any) => {
+    setInputPrecios({
+      ...inputPrecios,
+      [e.target.name]: Number(e.target.value),
+    });
+  };
+
+  //Calcula el total de la venta cuando es sacrificio
+  useEffect(() => {
+    if (isSacrificio) {
+      totalSacrificio();
+    }
+  }, [inputPrecios]);
 
   const corralesOptions = async () => {
     const response = await axios.get("/api/users/admin/ventas/corrales");
@@ -113,6 +148,12 @@ function VentasDashboard() {
     const response = await axios.post("/api/users/admin/ventas/corrales", {
       Corral: corralSelected?.Id,
     });
+    if (response.data[0][0].IdMotivo == 3) {
+      setIsSacrificio(true);
+      response.data[1].map((animal: any) => {
+        inputPrecios[animal.Arete] = Number(animal.PrecioSugerido);
+      });
+    }
     setAnimalesCorral(response.data[1]);
     setCorralInfo(response.data[0][0]);
     inputValue.precioFinal = response.data[0][0].PrecioNumber;
@@ -120,7 +161,6 @@ function VentasDashboard() {
 
   //Controla la venta del corral
   const handleVenta = async () => {
-    
     if (
       inputValue.precioFinal == 0 ||
       inputValue.psg == "" ||
@@ -137,29 +177,36 @@ function VentasDashboard() {
       });
       return;
     }
-    const response = await axios.post('/api/users/admin/ventas', {Corral: corralSelected?.Id, Reemo: CorralInfo?.REEMO, Precio: inputValue.precioFinal, Psg: inputValue.psg, Nombre: inputValue.nombre, Razon: inputValue.razonSocial, Municipio: inputValue.municipio, Localidad: inputValue.localidad, Estado: inputValue.estado});
-    if(response.status == 200){
-        setLastId(response.data[0][0].LastId);
-        toast({
-            title: "Venta realizada",
-            description: "La venta ha sido realizada correctamente",
-            variant: "success",
-        });
-        
-        
-    } else{
-        toast({
-            title: "Error",
-            description: "No se pudo realizar la venta",
-            variant: "destructive",
-        });
+    const response = await axios.post("/api/users/admin/ventas", {
+      Corral: corralSelected?.Id,
+      Reemo: CorralInfo?.REEMO,
+      Precio: inputValue.precioFinal,
+      Psg: inputValue.psg,
+      Nombre: inputValue.nombre,
+      Razon: inputValue.razonSocial,
+      Municipio: inputValue.municipio,
+      Localidad: inputValue.localidad,
+      Estado: inputValue.estado,
+    });
+    if (response.status == 200) {
+      setLastId(response.data[0][0].LastId);
+      toast({
+        title: "Venta realizada",
+        description: "La venta ha sido realizada correctamente",
+        variant: "success",
+      });
+    } else {
+      toast({
+        title: "Error",
+        description: "No se pudo realizar la venta",
+        variant: "destructive",
+      });
     }
   };
 
   useEffect(() => {
     if (lastId != 0) {
-    setIsNotPrinting(false);
-
+      setIsNotPrinting(false);
     }
   }, [lastId]);
 
@@ -171,7 +218,7 @@ function VentasDashboard() {
   }, [isNotPrinting]);
 
   useEffect(() => {
-    if (corralSelected?.Id != "") {
+    if (corralSelected != undefined) {
       getCorralVenta();
     }
   }, [corralSelected]);
@@ -183,10 +230,10 @@ function VentasDashboard() {
   return (
     <div className="h-[90vh] p-10 relative flex justify-center items-center">
       <SettingsModal />
-      <div className="border-2 border-black w-[60%] min-h-[60%] h-auto max-h-[95%] overflow-y-auto rounded-md p-5">
+      <div className="border-2 border-black w-[70%] min-h-[60%] h-auto max-h-[95%] overflow-y-auto rounded-md p-5">
         <p className="text-2xl font-bold text-center mb-2">
           {corralSelected
-            ? `Corral ${corralSelected?.Corral}`
+            ? `Corral ${corralSelected?.Corral} Motivo: ${CorralInfo?.Motivo}`
             : "Seleccione el corral a vender"}
         </p>
         {!isSelected && (
@@ -199,24 +246,33 @@ function VentasDashboard() {
         )}
         {corralSelected && (
           <div className="mt-5">
-            <div>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Inversión</th>
-                    <th>Precio Sugerido</th>
-                    <th>Ganancia</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>${CorralInfo?.Inversion}</td>
-                    <td>${CorralInfo?.Precio}</td>
-                    <td>${CorralInfo?.Ganancia}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            {((isSacrificio && !isSelected) || !isSacrificio) &&
+            corralSelected ? (
+              <div>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Inversión</th>
+                      <th>Precio Sugerido</th>
+                      <th>Ganancia</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>${CorralInfo?.Inversion}</td>
+                      <td>${CorralInfo?.Precio}</td>
+                      <td>${CorralInfo?.Ganancia}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              isSelected && (
+                <p className="text-center text-2xl">
+                  <strong>Total: </strong>${inputValue.precioFinal}
+                </p>
+              )
+            )}
             {!isSelected && (
               <div>
                 <p className="text-2xl font-bold text-center my-5">Animales</p>
@@ -231,6 +287,7 @@ function VentasDashboard() {
                         <th>Corral</th>
                         <th>Peso</th>
                         <th>Precio de Compra</th>
+                        {isSacrificio && <th>Precio Final</th>}
                       </tr>
                     </thead>
                     <tbody>
@@ -241,8 +298,19 @@ function VentasDashboard() {
                           <td>{animal.Meses}</td>
                           <td>{animal.Clasificacion}</td>
                           <td>{animal.Corral}</td>
-                          <td>{animal.Peso}</td>
+                          <td>{animal.Peso} Kg</td>
                           <td>${animal.PrecioCompra}</td>
+                          {isSacrificio && (
+                            <td>
+                              <input
+                                type="number"
+                                className="border border-black py-1 px-2 rounded-md w-36"
+                                defaultValue={animal.PrecioSugerido}
+                                name={animal.Arete}
+                                onChange={handlePrecioChange}
+                              />
+                            </td>
+                          )}
                         </tr>
                       ))}
                     </tbody>
@@ -250,7 +318,9 @@ function VentasDashboard() {
                 </div>
                 <button
                   className="px-[20%] py-2 font-semibold text-white bg-acento rounded hover:bg-acentohover mt-5 w-full"
-                  onClick={() => setisSelected(true)}
+                  onClick={() => {
+                    setisSelected(true);
+                  }}
                 >
                   Aceptar
                 </button>
@@ -260,23 +330,25 @@ function VentasDashboard() {
         )}
         {isSelected && (
           <div className="mt-5 gap-5 flex flex-col">
-            <div>
-              <label
-                htmlFor=""
-                className="font-bold text-lg flex-grow text-left"
-              >
-                Precio Final:
-              </label>
-              <input
-                type="number"
-                className="border border-black rounded-md w-full py-2 px-2"
-                placeholder="Ingrese el precio final de venta"
-                autoFocus
-                name="precioFinal"
-                defaultValue={CorralInfo?.PrecioNumber}
-                onChange={handleChange}
-              />
-            </div>
+            {!isSacrificio && (
+              <div>
+                <label
+                  htmlFor=""
+                  className="font-bold text-lg flex-grow text-left"
+                >
+                  Precio Final:
+                </label>
+                <input
+                  type="number"
+                  className="border border-black rounded-md w-full py-2 px-2"
+                  placeholder="Ingrese el precio final de venta"
+                  autoFocus
+                  name="precioFinal"
+                  defaultValue={CorralInfo?.PrecioNumber}
+                  onChange={handleChange}
+                />
+              </div>
+            )}
             <div>
               <label
                 htmlFor=""
@@ -395,26 +467,40 @@ function VentasDashboard() {
             <img src="/assets/Login/Logo.png" alt="LogoGanaderia" width={150} />
           </div>
           <div>
-          <p className="text-xl">
-              <strong>Venta:</strong>{lastId}
+            <p className="text-center font-bold text-3xl py-3">
+              Ganadería el Rosario
             </p>
             <p className="text-xl">
-              <strong>PSG:</strong>{inputValue.psg}
+              <strong>Venta:</strong>
+              {lastId}
             </p>
             <p className="text-xl">
-              <strong>Nombre:</strong>{inputValue.nombre}
+              <strong>Motivo:</strong>
+              {CorralInfo?.Motivo}
             </p>
             <p className="text-xl">
-              <strong>Razon Social:</strong>{inputValue.razonSocial}
+              <strong>PSG:</strong>
+              {inputValue.psg}
             </p>
             <p className="text-xl">
-              <strong>Municipio:</strong>{inputValue.municipio}
+              <strong>Nombre:</strong>
+              {inputValue.nombre}
             </p>
             <p className="text-xl">
-              <strong>Localidad:</strong>{inputValue.localidad}
+              <strong>Razon Social:</strong>
+              {inputValue.razonSocial}
             </p>
             <p className="text-xl">
-              <strong>Estado:</strong>{inputValue.estado}
+              <strong>Municipio:</strong>
+              {inputValue.municipio}
+            </p>
+            <p className="text-xl">
+              <strong>Localidad:</strong>
+              {inputValue.localidad}
+            </p>
+            <p className="text-xl">
+              <strong>Estado:</strong>
+              {inputValue.estado}
             </p>
           </div>
           <table className="my-5">
@@ -424,8 +510,10 @@ function VentasDashboard() {
                 <th>Sexo</th>
                 <th>Meses</th>
                 <th>Clas</th>
-                <th>Corral</th>
+                {!isSacrificio && <th>Corral</th>}
                 <th>Peso</th>
+                {isSacrificio && <th>Precio Final</th>}
+                {isSacrificio && <th>Precio por Kg</th>}
               </tr>
             </thead>
             <tbody>
@@ -435,8 +523,17 @@ function VentasDashboard() {
                   <td>{animal.Sexo}</td>
                   <td>{animal.Meses}</td>
                   <td>{animal.Clasificacion}</td>
-                  <td>{animal.Corral}</td>
+                  {!isSacrificio && <td>{animal.Corral}</td>}
                   <td>{animal.Peso}</td>
+                  {isSacrificio && <td>${inputPrecios[animal.Arete]}</td>}
+                  {isSacrificio && (
+                    <td>
+                      $
+                      {(
+                        inputPrecios[animal.Arete] / Number(animal.Peso)
+                      ).toFixed(2)}
+                    </td>
+                  )}
                 </tr>
               ))}
             </tbody>
